5353fbfd72e4e003e2a13b6e73f2f66e05276852
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-04-08 20:49:18 -0300

    Alteração na rota de curso não encontrado

--- src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -26,7 +26,7 @@ export class CursoDetalheComponent implements OnInit, OnDestroy{
     );
     this.curso = this.cursosService.getCurso(this.id);
     if (this.curso == null) {
-      this.router.navigate(['/cursonaoencontrado']);
+      this.router.navigate(['cursos/cursonaoencontrado']);
     }
   }
 
